21ae33d0ecf0faf434247cb59b5d0b9f07b31420
Rakibul Yeasin <rytotul@gmail.com>
2019-07-10 12:39:40 +0600

    Added Avro Installer

--- Bash/Avro_Installer.sh
+++ Bash/Avro_Installer.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Update Cache
+sudo apt update -y
+
 # Install the Must have packages
-sudo apt install -y git libibus-1.0-0 libibus-1.0-dev ibus automake autoconf gjs gir1.2-gjsdbus-1.0 gir1.2-ibus-1.0
+sudo apt install -y git libibus-1.0-dev ibus autotools-dev automake autoconf gjs gir1.2-ibus-1.0
 
 git clone git://github.com/sarim/ibus-avro.git
 cd ibus-avro
