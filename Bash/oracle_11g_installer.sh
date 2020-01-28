f3e5c9fc8a57b620b38214b9e939b53f5ab1913f
Rakibul Yeasin <rytotul@gmail.com>
2020-01-28 22:47:05 +0600

    Update oracle_11g_installer.sh

--- Bash/oracle_11g_installer.sh
+++ Bash/oracle_11g_installer.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-echo -e "Installing Oracle 11g XE..."
+echo -e "[+] Installing Oracle 11g XE..."
 
 
 # unzip oracle-xe-11.2.0-1.0.x86_64.rpm.zip
-# sudo apt-get install alien libaio1 unixodbc
+sudo apt-get install alien libaio1 unixodbc
 # sudo alien --scripts -d oracle-*.rpm
 # sudo alien -i --scripts oracle-*.rpm
 
-echo -e "ALien..."
 sudo touch /sbin/chkconfig
-echo -e "Config creation success..."
+echo -e "[+] Writing the following config to Configuration file...\n"
 echo '
 #!/bin/bash
 # Oracle 11gR2 XE installer chkconfig hack for Ubuntu
@@ -29,25 +28,25 @@ fi
 update-rc.d oracle-xe defaults 80 01' | sudo tee /sbin/chkconfig
 
 
-echo -e "Done!"
+echo -e "n[+] Done!"
 sudo chmod 755 /sbin/chkconfig
 
-echo -e "Changing Kernel Parameters..."
-echo "# Oracle 11g XE kernel parameters
+echo -e "[+] Changing Kernel Parameters...\n"
+echo '# Oracle 11g XE kernel parameters
 fs.file-max=6815744
 net.ipv4.ip_local_port_range=9000 65000
 kernel.sem=250 32000 100 128
-kernel.shmmax=536870912" | sudo tee /etc/sysctl.d/60-oracle.conf
+kernel.shmmax=536870912' | sudo tee /etc/sysctl.d/60-oracle.conf
 
 sudo service procps start
 
-echo -e "Kinda rebooting..."
+echo -e "\n[+] Kinda rebooting..."
 sudo sysctl -q fs.file-max
 
-echo -e "Writing loader script..."
+echo -e "[+] Writing loader script...\n"
 
-echo "#!/bin/sh
-case '$1' in
+echo '#!/bin/sh
+case "$1" in
 start)
     mkdir /var/lock/subsys 2>/dev/null
     touch /var/lock/subsys/listener
@@ -57,9 +56,11 @@ start)
     echo error
     exit 1
     ;;
-esac" | sudo tee /etc/rc2.d/S01shm_load
+esac' | sudo tee /etc/rc2.d/S01shm_load
 
 sudo chmod 755 /etc/rc2.d/S01shm_load
+
+echo "[+] Symlincing the system..."
 
 sudo ln -s /usr/bin/awk /bin/awk
 sudo mkdir /var/lock/subsys
@@ -67,18 +68,25 @@ sudo touch /var/lock/subsys/listener
 
 sysctl -p /etc/sysctl.d/60-oracle.conf
 
-sudo dpkg --install oracle-xe_11.2.0-2_amd64.deb
+echo -e "[+] Now installing the database..."
+
+sudo alien -i --scripts oracle-*.rpm
+# sudo dpkg --install oracle-xe_11.2.0-2_amd64.deb
 sudo /etc/init.d/oracle-xe configure
 
-echo "export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+echo -e "[+] Writing Oracle Configurations..."
+
+echo 'export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 export ORACLE_SID=XE
 export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh`
 export ORACLE_BASE=/u01/app/oracle
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
-export PATH=$ORACLE_HOME/bin:$PATH" | sudo tee -a ~/.bashrc
+export PATH=$ORACLE_HOME/bin:$PATH' | sudo tee -a ~/.bashrc
 
 source ~/.bashrc
 
 sudo service oracle-xe start
 
 sudo usermod -a -G dba $USER
+
+echo "\n[+] Good Luck..."
