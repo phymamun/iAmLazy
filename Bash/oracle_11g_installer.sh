6c223783af6885b16816f1234c572c51b9d3fe29
Rakibul Yeasin <rytotul@gmail.com>
2020-01-28 22:35:48 +0600

    Update oracle_11g_installer.sh

--- Bash/oracle_11g_installer.sh
+++ Bash/oracle_11g_installer.sh
@@ -11,25 +11,22 @@ echo -e "Installing Oracle 11g XE..."
 echo -e "ALien..."
 sudo touch /sbin/chkconfig
 echo -e "Config creation success..."
-PTH_CONFIG=$(cat <<END
+echo '
 #!/bin/bash
 # Oracle 11gR2 XE installer chkconfig hack for Ubuntu
 file=/etc/init.d/oracle-xe
 if [[ ! `tail -n1 $file | grep INIT` ]]; then
     echo >> $file
-    echo '### BEGIN INIT INFO' >> $file
-    echo '# Provides: OracleXE' >> $file
-    echo '# Required-Start: $remote_fs $syslog' >> $file
-    echo '# Required-Stop: $remote_fs $syslog' >> $file
-    echo '# Default-Start: 2 3 4 5' >> $file
-    echo '# Default-Stop: 0 1 6' >> $file
-    echo '# Short-Description: Oracle 11g Express Edition' >> $file
-    echo '### END INIT INFO' >> $file
+    echo "### BEGIN INIT INFO" >> $file
+    echo "# Provides: OracleXE" >> $file
+    echo "# Required-Start: $remote_fs $syslog" >> $file
+    echo "# Required-Stop: $remote_fs $syslog" >> $file
+    echo "# Default-Start: 2 3 4 5" >> $file
+    echo "# Default-Stop: 0 1 6" >> $file
+    echo "# Short-Description: Oracle 11g Express Edition" >> $file
+    echo "### END INIT INFO" >> $file
 fi
-update-rc.d oracle-xe defaults 80 01
-END
-)
-echo $PTH_CONFIG # | sudo tee /sbin/chkconfig
+update-rc.d oracle-xe defaults 80 01' | sudo tee /sbin/chkconfig
 
 
 echo -e "Done!"
