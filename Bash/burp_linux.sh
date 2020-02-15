b6fc16b25d4bd713f928a160f445f5499c42f8f9
Rakibul Yeasin <rytotul@gmail.com>
2020-02-15 18:06:28 +0600

    :fire: :heart:

--- Bash/burp_linux.sh
+++ Bash/burp_linux.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-cat <"END"
-    \# You need to download Oracle-JDK-8 First
-    \# Otherwise this package may not work
+cat << "END"
+    # You need to download Oracle-JDK-8 First
+    # Otherwise this package may not work
 END
 
 install() {
     sudo mkdir -p /opt/jdk
     sudo cp -rf jdk-8* /opt/jdk/
-    sudo tar -zxf /opt/jdk/jdk-8*.gz
-    sudo rm /opt/jdk/jdk-8*.gz
-    sudo cp -r jdk1.8.0_241/* .
+    sudo tar -C /opt/jdk/ -zxf /opt/jdk/jdk-8*
+    sudo cp -r /opt/jdk/jdk1.8.*/* /opt/jdk/
+    sudo rm -rf /opt/jdk/jdk1.8.*
     /opt/jdk/bin/java -jar burp-loader.jar &
-    /opt/jdk/bin/java -noverify -Xbootclasspath/p:burp-loader.jar -jar burpsuite_pro_v2020.1.jar
+    /opt/jdk/bin/java -noverify -Xbootclasspath/p:burp-loader.jar -jar burpsuite_pro_v2020.1.jar &
 }
 
 if [[ $1 == 'i' ]]; then
     install
 else
-    /opt/jdk/bin/java -noverify -Xbootclasspath/p:burp-loader.jar -jar burpsuite_pro_v2020.1.jar > /dev/null
+    /opt/jdk/bin/java -noverify -Xbootclasspath/p:burp-loader.jar -jar burpsuite_pro_v2020.1.jar &
     # /opt/jdk/bin/java -noverify -Xbootclasspath/p:burp-loader.jar -jar burpsuite_pro_v2020.1.jar 2> $1
 fi
